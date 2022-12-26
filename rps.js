4fd815bfb9989b35592e4b2186725e144d3650b5
Joshua Menko <catch2two@Xubuntu.myguest.virtualbox.org>
2022-12-26 15:22:18 -0500

    Aded getComputerChoice Function

--- rps.js
+++ rps.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+// User is asked to Choose Rock, Paper, or Scissors
+// Computer Chooses randomly between Rock, Paper, or Scissors
+// if user chooses Rock, let user win if computer Chose Scissors
+//  else computer wins if Paper
+// if User chooses Paper, let user win if computer chose Rock
+//  else computer wins if Scissors
+// if user chooses Scissors, let user win if computer chose Paper
+//  else computer wins if rock
+// Else if = then both Tie
+
+
+
+function getComputerChoice(choice) {    
+    const choice = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * choice.length);
+ console.log(random, choice[random]);
+}
+ 
+
+function playRound(playerSelection, computerSelection) {
+    let 
+}
