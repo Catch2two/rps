ca8ee949ea01a8a9102aef2c295fd021eac612a9
Joshua Menko <catch2two@Xubuntu.myguest.virtualbox.org>
2022-12-26 18:21:00 -0500

    Added intermediate skill version, it uses switch for Function

--- rps.js
+++ rps.js
@@ -8,15 +8,59 @@
 //  else computer wins if rock
 // Else if = then both Tie
 
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
 
+let userChoice
+let computerChoice
+let result
 
-function getComputerChoice(choice) {    
-    const choice = ['rock', 'paper', 'scissors'];
-    const random = Math.floor(Math.random() * choice.length);
- console.log(random, choice[random]);
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor (Math.random() * 3) + 1 // or possibleChoices.length
+    console.log(randomNumber)
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
 }
- 
 
-function playRound(playerSelection, computerSelection) {
-    let 
-}
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'its a draw'
+    }
+    if (computerChoice === 'rock' && userChoice === "scissors") {
+        result = 'you lost!'
+    }
+        if (computerChoice === 'paper' && userChoice === "scissors") {
+        result = 'you win!'
+        }
+        if (computerChoice === 'scissors' && userChoice === "rock") {
+        result = 'you win!'
+        }
+        if (computerChoice === 'scissors' && userChoice === "paper") {
+        result = 'you lost!'
+        }
+        if (computerChoice === 'rock' && userChoice === "paper") {
+        result = 'you win!'
+        }
+        if (computerChoice === 'paper' && userChoice === "rock") {
+        result = 'you lose!'
+    }
+    resultDisplay.innerHTML = result
+    }
